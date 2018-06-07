06323b5a29bc1cc7ed57bca1e342240539cfa3ce
pakuyuya <pakuyuya@yahoo.co.jp>
2018-06-07 23:50:58 +0900

    add jobitem/sql/GetSQLfromMap template

--- jobitem/sql.go
+++ jobitem/sql.go
@@ -1,6 +1,7 @@
 package jobitem
 
 import (
+	"bytes"
 	"errors"
 	"strings"
 )
@@ -16,4 +17,52 @@ func GetSQLfromDBRow(node *Node) (string, error) {
 	}
 
 	return "", errors.New(`not found <elementparameter name="QUERY" />`)
+}
+
+func GetSQLfromMap(node *Node) (string, error) {
+	inputs, err := _getInputColumns(node)
+	if err != nil {
+		return "", err
+	}
+
+	outputs, err := _getOutputColumns(node)
+	if err != nil {
+		return "", err
+	}
+
+	return _buildSQL(inputs, outputs)
+}
+
+type _ColumnInfo struct {
+	Scheme     string
+	TableName  string
+	TableAlias string
+	Join       bool
+	JoinType   string
+	Expression string
+	Operator   string
+}
+
+func _getInputColumns(node *Node) ([]_ColumnInfo, error) {
+	columns := []_ColumnInfo{}
+
+	// TODO: implements
+
+	return columns, nil
+}
+
+func _getOutputColumns(node *Node) ([]_ColumnInfo, error) {
+	columns := []_ColumnInfo{}
+
+	// TODO: implements
+
+	return columns, nil
+}
+
+func _buildSQL(inputs []_ColumnInfo, outputs []_ColumnInfo) (string, error) {
+	var b bytes.Buffer
+
+	// TODO: implements
+
+	return b.String(), nil
 }
